3b7e292e86587de5dd91c7b85359956f7c4af253
Glaswartron <benedikt.wille.privat@googlemail.com>
2023-07-07 21:40:25 +0200

    Player Movement Grundlagen

--- GMTK-Game-Jam-2023/Assets/Player.cs
+++ GMTK-Game-Jam-2023/Assets/Player.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 1;
+    public float accelerationTime = 0.5f;
+
+    private Vector2 movement;
+    private float speedIncrement;
+
+    private float accelerationStartTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = Vector2.zero; // (0, 0)
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            accelerationStartTime = Time.fixedTime;
+            speedIncrement = 0;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement = Vector2.left; // (-1, 0)
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            movement = Vector2.right; // (0, 1)
+        }
+        else
+        {
+            movement = Vector2.zero; // (0, 0)
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movement *= 2;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float t = Time.fixedTime - accelerationStartTime;
+        speedIncrement = SpeedFunction(t, accelerationTime);
+
+        transform.Translate(movement * speed * speedIncrement * Time.fixedDeltaTime);
+    }
+
+    private float SpeedFunction(float t, float accelerationTime)
+    {
+        if (t >= accelerationTime)
+            return 1f;
+
+        return BaseSpeedFunction(t) / BaseSpeedFunction(accelerationTime);
+    }
+
+    private float BaseSpeedFunction(float x)
+    {
+        return Mathf.Pow(x, 2);
     }
 }
