88305ed9e0c2d9a6f3e6f49698f1b73fc7d5f5c4
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-07-08 09:45:44 +0200

    Jumping Rate for Jumping Enemies

--- GMTK-Game-Jam-2023/Assets/DirectionJumpingEnemy.cs
+++ GMTK-Game-Jam-2023/Assets/DirectionJumpingEnemy.cs
@@ -11,6 +11,7 @@ public class DirectionJumpingEnemy : Enemy
     public float jumpRate;
 
     private float jumpStartTime;
+    private float lastJump;
 
     private bool jumping = false;
 
@@ -18,6 +19,7 @@ public class DirectionJumpingEnemy : Enemy
     void Start()
     {
         base.Start();
+        lastJump = Time.fixedTime;
     }
 
     // Update is called once per frame
@@ -29,15 +31,12 @@ public class DirectionJumpingEnemy : Enemy
         {
             if(!jumping)
             {
-                if(IsGrounded())
+                if(IsGrounded()) //Am Boden
                 {
-                    jumpStartTime = Time.fixedTime;
-                    jumping = true;
-
-                    enemyRigidBody.velocity = Vector2.up * jumpingPower;
+                    Jump();
                 }
             }
-            else
+            else //jumping
             {
                 if (IsGrounded() && Time.fixedTime - jumpStartTime > 0.2f)
                 {
@@ -46,9 +45,38 @@ public class DirectionJumpingEnemy : Enemy
                 }
             }
         }
-        else
+        else if(!jumpAlways) //else w¸rde reichen, aber lesbarkeit
         {
-
+            if(!jumping)
+            {
+                if(IsGrounded())
+                {
+                    if(Time.fixedTime - lastJump >= jumpRate)
+                    {
+                        Jump();
+                    }
+                }
+            }
+            else
+            {
+                if(IsGrounded() && Time.fixedTime - jumpStartTime > 0.2f)
+                {
+                    jumping = false;
+                    lastJump = Time.fixedTime;
+                }
+            }
         }
+    }
+
+    /// <summary>
+    /// Popul‰rer Song von Van Halen aus dem Jahr 1984. Der Song erschien erstmals auf dem gleichnamigen Album 1984. Ja, das Album heiﬂt 1984.
+    /// 1984 ist das sechste Album der amerikanischen Hard-Rock Band Van Halen.Jump ist der zweite Song des Alums, der Song geht 4 Minuten und 4 Sekunden lang
+    /// </summary>
+    protected void Jump()
+    {
+        jumpStartTime = Time.fixedTime;
+        jumping = true;
+
+        enemyRigidBody.velocity = Vector2.up * jumpingPower;
     }
 }
