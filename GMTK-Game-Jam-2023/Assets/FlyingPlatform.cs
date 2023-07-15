d66f4d4dd395f57ab52646eaac029256b118f45d
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-07-15 17:05:56 +0200

    Torben

--- GMTK-Game-Jam-2023/Assets/FlyingPlatform.cs
+++ GMTK-Game-Jam-2023/Assets/FlyingPlatform.cs
@@ -10,7 +10,12 @@ public class FlyingPlatform : MonoBehaviour
     public Transform end;
     public float speed;
 
+    public bool waitAtPoints;
+    public float waitingTime;
+
     private Transform target;
+    private bool waiting = false;
+    private bool semaphora = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +28,28 @@ public class FlyingPlatform : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        platform.position = Vector2.MoveTowards(platform.position, target.position, speed * Time.fixedDeltaTime);
+        if(!waiting)
+            platform.position = Vector2.MoveTowards(platform.position, target.position, speed * Time.fixedDeltaTime);
 
         if (Vector2.SqrMagnitude(target.position - platform.position) < 0.05f)
         {
-            target = target == end ? start : end;
+            if (waitAtPoints && !semaphora)
+            {
+                waiting = true;
+                semaphora = true;
+                StartCoroutine(WaitAtPoint());
+            }
+            if (!waiting)
+            {
+                target = target == end ? start : end;
+                semaphora = false;
+            }
         }
+    }
+
+    private IEnumerator WaitAtPoint()
+    {
+        yield return new WaitForSeconds(waitingTime);
+        waiting = false;
     }
 }
