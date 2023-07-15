68cacba0c93c54bcfa643bbd5a6351c40509b6c8
BelmontR <47500226+BelmontR@users.noreply.github.com>
2023-07-15 23:31:40 +0200

    Started Bugfix

--- GMTK-Game-Jam-2023/Assets/BlockDownHitbox.cs
+++ GMTK-Game-Jam-2023/Assets/BlockDownHitbox.cs
@@ -8,6 +8,20 @@ public class BlockDownHitbox : MonoBehaviour
     {
         if(collision.tag == "BlockBreaker")
         {
+
+            if (collision.GetComponentInParent<Player>() != null)
+            {
+                Debug.Log("Players y: " + Mathf.Abs(collision.transform.position.y));
+                Debug.Log("Eigenes global y: " + Mathf.Abs(this.transform.parent.position.y));
+                Debug.Log("Eigenes lokales y: " + Mathf.Abs(this.transform.localPosition.y));
+
+                if ((collision.transform.position.y) > (this.transform.parent.position.y))
+                {
+                    Debug.Log("ALARM!!!");
+                    return;
+                }
+
+            }
             var block = GetComponentInParent<Block>();
             if (block.destructable)
             {
